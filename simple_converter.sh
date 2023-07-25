7962b1d6e3ce8012b0e55367724e7915871114ed
Omar Ahmad <dev.omar.taher@gmail.com>
2023-07-25 14:33:22 +0300

    Stage 3/5

--- simple_converter.sh
+++ simple_converter.sh
@@ -2,51 +2,28 @@
 
 # write your code here
 
-is_number() {
-	re_constant="^\-?[0-9]+\.?[0-9]*$"
-	if [[ $1 =~ $re_constant ]]; then
-		echo 1
-	else
-		echo 0
-	fi
+print_menu() {
+	echo "Select an option"
+	echo "0. Type '0' or 'quit' to end program"
+	echo "1. Convert units"
+	echo "2. Add a definition"
+	echo "3. Delete a definition"
 }
 
-is_definition() {
-	re_definition="^[a-z]+_to_[a-z]+$"
-	if [[ $1 =~ $re_definition ]]; then
-		echo 1
-	else
-		echo 0
-	fi
-}
+echo -e "Welcome to the Simple converter!\n"
+while true;
+do
+	print_menu
+	read -r option
+	case "$option" in
+		"1"|"2"|"3")
+			echo -e "Not implemented!\n";;
 
-is_input_correct() {
-	if [ $1 -eq 2 ] && [ $(is_definition $2) -eq 1 ] && [ $(is_number $3) -eq 1 ]; then
-		echo 1
-	else
-		echo 0
-	fi
-}
-
-echo "Enter a definition:"
-read -a user_input
-
-arr_length="${#user_input[@]}"
-definition="${user_input[0]}"
-constant="${user_input[1]}"
-
-result=$(is_input_correct $arr_length $definition $constant)
-if [ $result -eq 1 ]; then
-	echo "Enter a value to convert:"
-	read value
-	while [ $(is_number $value) -ne 1 ]
-	do
-		echo "Enter a float or integer value!"
-   		read value
-	done
-	r=$(bc -l <<< "$constant * $value")
-	echo "Result:" $r
-	
-else
-	echo "The definition is incorrect!"
-fi
+		"0" | "quit")
+			echo "Goodbye!"
+			break
+			;;
+		*)
+			echo -e "Invalid option!\n";;
+	esac
+done
